c05ec72981f380b5e64c822b202f8b090d482544
Privacy_Dragon <privacy_dragon@tutanota.com>
2021-03-26 12:05:17 +0100

    Add files via upload

---
+++ BotsToGmi.py
@@ -0,0 +1,19 @@
+import json, os
+
+def main():
+	os.system("curl https://codeberg.org/privacy_dragon/pages/raw/branch/main/bots.json --output \"json.txt\"")
+	blup = open("json.txt")
+	blupblup = blup.read()
+	blup.close()
+	blupblupblup = blupblup.replace("\n", "")
+	fish = json.loads(blupblupblup)
+	bluppingFish = []
+	bluppingFish.append("#Gemini bots and proxy's\n\n")
+	for item in fish:    #------------------------------------------------------------------V Let the links show as links.
+		bluppingFish.append("##{0}:\nIPv6: {1}\nStatic: {2}\nType: {3}\nDescription: {4}\n=> {5}\n".format(item["IPv4"], item["IPv6"], item["Static"], item["Type"], item["Description"], item["Website"]))
+	bluppingFish = "".join(bluppingFish)
+	blah = open("main/bots.gmi", "w")
+	blah.write(bluppingFish)
+	blah.close()
+	return bluppingFish
+	
